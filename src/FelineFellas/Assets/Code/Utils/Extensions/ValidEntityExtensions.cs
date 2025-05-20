76f27374ef53d7c4bbd67315069cd095dd4e9b04
rapuska <mikalajsiatkouski@gmail.com>
2025-05-21 00:33:08 +0300

    add debugger

---
+++ src/FelineFellas/Assets/Code/Utils/Extensions/ValidEntityExtensions.cs
@@ -0,0 +1,12 @@
+using Entitas.Generic;
+
+namespace FelineFellas
+{
+    public static class ValidEntityExtensions
+    {
+        public static bool IsAlive(this Entity<GameScope> @this)
+            => @this.isEnabled
+                && !@this.Is<Dead>()
+                && !@this.Is<Destroy>();
+    }
+}
